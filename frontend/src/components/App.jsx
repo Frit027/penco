102328521e2325b2900aeeb4acb526bd2fa3ae05
Frit027 <Frit_027@mail.ru>
2024-05-01 17:06:58 +0300

    created test component with socket emitting

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
+
+export const App = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        socket.on('click', (data) => setCount(data.count));
+    }, []);
+
+    useEffect(() => {
+        socket.emit('click', { count })
+    }, [count]);
+
+    const handleClick = () => setCount((prevCount) => prevCount + 1);
+
+    return (
+        <div>
+            <button onClick={handleClick}>Click</button>
+            <span>{count}</span>
+        </div>
+    );
+};
