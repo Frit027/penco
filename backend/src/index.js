f1b8f426b5d9c6ac511fe1ded133379a6f3f4d22
Frit027 <Frit_027@mail.ru>
2024-05-01 17:04:49 +0300

    created server with test socket emitting

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin:"http://localhost:3000",
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log(`a user ${socket.id} connected`);
+    socket.on('disconnect', () => {
+        console.log(`user ${socket.id} disconnected`);
+    });
+
+    socket.on('click', (data) => {
+        socket.broadcast.emit('click', data);
+    });
+});
+
+server.listen(4000, () => console.log('Server running at http://localhost:4000'));
