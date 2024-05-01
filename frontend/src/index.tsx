ca6cb0e69a95e711d9077d2852454fc5d4d96cd8
Frit027 <Frit_027@mail.ru>
2024-05-01 18:28:36 +0300

    migrated to TypeScript

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root') as HTMLDivElement);
 root.render(<App/>);
