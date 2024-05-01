e87950741d607297008275c082f694da282f0970
Frit027 <Frit_027@mail.ru>
2024-05-01 19:40:38 +0300

    created eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb-typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.eslint.json',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', 4],
+        'no-console': ['warn', { allow: ['log'] }],
+        'import/prefer-default-export': 0,
+        '@typescript-eslint/indent': ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/function-component-definition': [2, {
+            namedComponents: 'arrow-function',
+            unnamedComponents: 'arrow-function',
+        }],
+    },
+};
