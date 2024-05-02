2d325739ad735a6841f206daa0b66c8692f63bab
Frit027 <Frit_027@mail.ru>
2024-05-02 21:46:01 +0300

    improved code readability

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -105,5 +105,12 @@ export const App = () => {
     }, [handleMouseUp]);
 
     console.log('render');
-    return <canvas ref={canvasRef} width="600" height="600" style={{ backgroundColor: '#eee', border: '1px solid #ccc', margin: '10px' }} />;
+    return (
+        <canvas
+            ref={canvasRef}
+            width="600"
+            height="600"
+            style={{ backgroundColor: '#eee', border: '1px solid #ccc', margin: '10px' }}
+        />
+    );
 };
