c5add32d8aa91e78f21bb9b21d3f8f01ac787ba7
Frit027 <Frit_027@mail.ru>
2024-05-10 12:57:55 +0300

    added Toolbar and handler for change figure

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,4 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Toolbar } from './toolbar';
 import { Canvas } from './canvas';
+import { Figure } from '../interfaces';
 
-export const App = () => <Canvas />;
+export const App = () => {
+    const [figureType, setFigureType] = useState<Figure | null>(null);
+
+    /**
+     * Handler for click to change a figure type to be drawn
+     * @param {Figure} selectedFigureType - Type of figure to be drawn
+     */
+    const handleChangeFigureType = (selectedFigureType: Figure) => setFigureType(selectedFigureType);
+
+    return (
+        <>
+            <Toolbar changeFigureType={handleChangeFigureType} />
+            <Canvas figureType={figureType} />
+        </>
+    );
+};
