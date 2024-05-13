b6f61270311c606897e9ceb66292052b3a511930
Frit027 <Frit_027@mail.ru>
2024-05-13 18:33:37 +0300

    methods and markup moved to MainPage component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,21 +1,4 @@
-import React, { useState } from 'react';
-import { Toolbar } from './toolbar';
-import { Canvas } from './canvas';
-import { Figure } from '../interfaces';
+import React from 'react';
+import { MainPage } from '../pages/main-page';
 
-export const App = () => {
-    const [figureType, setFigureType] = useState<Figure | null>(null);
-
-    /**
-     * Handler for click to change a figure type to be drawn
-     * @param {Figure} selectedFigureType - Type of figure to be drawn
-     */
-    const handleChangeFigureType = (selectedFigureType: Figure) => setFigureType(selectedFigureType);
-
-    return (
-        <>
-            <Toolbar changeFigureType={handleChangeFigureType} />
-            <Canvas figureType={figureType} />
-        </>
-    );
-};
+export const App = () => <MainPage />;
