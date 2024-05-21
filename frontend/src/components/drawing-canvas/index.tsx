8444f68ab4e94ed3cf8ad45895f95f19620f9e4d
Frit027 <Frit_027@mail.ru>
2024-05-21 20:08:02 +0300

    when resizing the window, the drawn content doesn't disappear

--- frontend/src/components/drawing-canvas/index.tsx
+++ frontend/src/components/drawing-canvas/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
 import { FigureTypeContext, TFigureTypeContext } from '../../contexts';
 import { useDrawingLine, useDrawingRectangle, useDrawingCircle } from '../../hooks';
 
@@ -9,10 +9,41 @@ export const DrawingCanvas = () => {
     const { figureType } = useContext(FigureTypeContext) as TFigureTypeContext;
     const fakeCanvasRef = useRef<HTMLCanvasElement>(null);
     const originCanvasRef = useRef<HTMLCanvasElement>(null);
+    const [content, setContent] = useState<ImageData | null>(null);
 
     useDrawingLine(fakeCanvasRef, originCanvasRef, figureType);
     useDrawingRectangle(fakeCanvasRef, originCanvasRef, figureType);
     useDrawingCircle(fakeCanvasRef, originCanvasRef, figureType);
+
+    const saveContent = () => {
+        const originCanvas = originCanvasRef.current;
+        if (!originCanvas) {
+            return;
+        }
+
+        const originContext = originCanvas.getContext('2d');
+        if (!originContext) {
+            return;
+        }
+
+        setContent(originContext.getImageData(0, 0, originCanvas.width, originCanvas.height));
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', saveContent);
+        return () => window.removeEventListener('resize', saveContent);
+    }, []);
+
+    useEffect(() => {
+        const originCanvas = originCanvasRef.current;
+        const originContext = originCanvas?.getContext('2d');
+
+        if (!content) {
+            return;
+        }
+
+        originContext?.putImageData(content, 0, 0);
+    }, [content]);
 
     return (
         <div>
@@ -26,7 +57,7 @@ export const DrawingCanvas = () => {
                 ref={fakeCanvasRef}
                 width={window.innerWidth}
                 height={window.innerHeight}
-                style={{ position: 'absolute', top: 0, left: 0 }}
+                style={{ position: 'fixed', top: 0, left: 0 }}
             />
         </div>
     );
