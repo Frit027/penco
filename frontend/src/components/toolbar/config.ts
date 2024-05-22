43652ad31fa46005c75e1ac522ffee5acf92685d
Frit027 <Frit_027@mail.ru>
2024-05-22 17:22:06 +0300

    added css-classname for button

--- frontend/src/components/toolbar/config.ts
+++ frontend/src/components/toolbar/config.ts
@@ -1,9 +1,10 @@
 export const classes = {
     component: 'toolbar',
-    item: 'toolbar__item',
-    selectedItem: 'toolbar__item_selected',
-    line: 'toolbar__item_line',
-    rectangle: 'toolbar__item_rectangle',
-    circle: 'toolbar__item_circle',
-    pdf: 'toolbar__item_pdf',
+    button: 'toolbar__button',
+    selected: 'toolbar__button_selected',
+    icon: 'toolbar__icon',
+    line: 'toolbar__icon_line',
+    rectangle: 'toolbar__icon_rectangle',
+    circle: 'toolbar__icon_circle',
+    pdf: 'toolbar__icon_pdf',
 };
