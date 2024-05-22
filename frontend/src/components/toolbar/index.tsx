955666d01646b6fe71d32b6d242e30ed40d401b0
Frit027 <Frit_027@mail.ru>
2024-05-22 17:23:42 +0300

    добавлены значки внутри кнопок

--- frontend/src/components/toolbar/index.tsx
+++ frontend/src/components/toolbar/index.tsx
@@ -1,19 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import classNames from 'classnames';
 import { Figure } from '../../interfaces';
 import { FigureTypeContext, TFigureTypeContext } from '../../contexts';
 import { FileUploader } from '../file-uploader';
+import { classes } from './config';
+import './styles.less';
 
 /**
  * Component for displaying a toolbar
  */
 export const Toolbar = () => {
     const { setFigureType } = useContext(FigureTypeContext) as TFigureTypeContext;
+    const [selectedTool, setSelectedTool] = useState<Figure | null>(null);
+
+    const changeCurrentTool = (figureType: Figure) => {
+        setSelectedTool(figureType);
+        setFigureType(figureType);
+    };
 
     return (
-        <div style={{ position: 'absolute', zIndex: 1 }}>
-            <button type="button" onClick={() => setFigureType(Figure.Line)}>Line</button>
-            <button type="button" onClick={() => setFigureType(Figure.Rectangle)}>Rect</button>
-            <button type="button" onClick={() => setFigureType(Figure.Circle)}>Circle</button>
+        <div className={classes.component}>
+            <button
+                className={classNames(classes.button, {
+                    [classes.selected]: selectedTool === Figure.Line,
+                })}
+                type="button"
+                aria-label="Line"
+                onClick={() => changeCurrentTool(Figure.Line)}
+            >
+                <span className={classNames(classes.icon, classes.line)} />
+            </button>
+            <button
+                className={classNames(classes.button, {
+                    [classes.selected]: selectedTool === Figure.Rectangle,
+                })}
+                type="button"
+                aria-label="Rectangle"
+                onClick={() => changeCurrentTool(Figure.Rectangle)}
+            >
+                <span className={classNames(classes.icon, classes.rectangle)} />
+            </button>
+            <button
+                className={classNames(classes.button, {
+                    [classes.selected]: selectedTool === Figure.Circle,
+                })}
+                type="button"
+                aria-label="Circle"
+                onClick={() => changeCurrentTool(Figure.Circle)}
+            >
+                <span className={classNames(classes.icon, classes.circle)} />
+            </button>
             <FileUploader />
         </div>
     );
