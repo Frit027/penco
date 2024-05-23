a6a9157342e86691bef720df4d4161a0226505b5
Frit027 <Frit_027@mail.ru>
2024-05-23 14:28:47 +0300

    created constants of css-class names for pdf-canvas

---
+++ frontend/src/components/pdf-canvas/config.ts
@@ -0,0 +1,8 @@
+export const classes = {
+    component: 'pdf-canvas',
+    firstCanvas: 'pdf-canvas_first',
+    secondCanvas: 'pdf-canvas_second',
+    thirdCanvas: 'pdf-canvas_third',
+    originCanvas: 'pdf-canvas_origin',
+    fakeCanvas: 'pdf-canvas_fake',
+};
